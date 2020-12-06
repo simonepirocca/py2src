b456e9e7929ff8f2df8822e140e36f5bf256b97a
vuduclyunitn <ducly.vu@unitn.it>
2020-12-06 08:19:20 -0500

    Adds the log function

--- tests/test_ossgadget1.py
+++ tests/test_ossgadget1.py
@@ -2,12 +2,17 @@ import sys
 import os
 import logging
 import subprocess
-logging.basicConfig(filename="log.log", level=logging.INFO)
+import pytest
+from pathlib import Path
 
+utils_module_path = Path().resolve().parent / "src" / "utils"
+sys.path.append(str(utils_module_path))
+from utils import log_function_output
+
+logger = log_function_output(file_level=logging.DEBUG, console_level=logging.DEBUG, log_filename="log_test.log")
 def test():
     package_name = "urllib3"
     command = ["docker", "run", "-it", "ossgadget:latest", "/bin/bash", "-c", "./oss-find-source/bin/Debug/netcoreapp3.1/oss-find-source pkg:pypi/" + package_name]
     result = subprocess.run(command, stdout=subprocess.PIPE)
     url = result.stdout.decode('utf-8')
-
-    logging.info(f"{url}")
+    logger.info(f"{url}")
