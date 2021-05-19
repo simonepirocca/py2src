2c1ffee7343fd6c3b422696954ba7b87d7853eb5
Simone Pirocca <simone.pirocca@gmail.com>
2021-05-19 13:49:10 +0200

    Fix

--- src/clone_repository.sh
+++ src/clone_repository.sh
@@ -3,7 +3,9 @@ CLONE_URL=$1
 CLONE_DIR=$2
 if [[ ! -z "$CLONE_URL" ]] && [[ ! -z "$CLONE_DIR" ]]
 then
-    cd ../cloned_repos 
+    cd ..
+    if [[ ! -z "cloned_repos" ]]; then mkdir cloned_repos; fi
+    cd cloned_repos 
     if [ -f "./$CLONE_DIR" ]; then rm ../$CLONE_DIR; fi
     git clone -n $CLONE_URL 
     echo "CLONED" 
